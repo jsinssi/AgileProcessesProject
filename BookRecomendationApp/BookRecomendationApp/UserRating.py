c249f3b7dec49183ecda44d6516344e4c0164dfe
Martina Martin <marti.law@gmail.com>
2025-11-12 13:35:09 +0000

    Added connection to Mongo Database

--- BookRecomendationApp/BookRecomendationApp/UserRating.py
+++ BookRecomendationApp/BookRecomendationApp/UserRating.py
@@ -2,6 +2,8 @@ import pandas as pd
 from typing import List
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from pymongo import MongoClient
+
 
 app = FastAPI(title="User Rated Books API")
 
@@ -12,8 +14,11 @@ class UserRatings(BaseModel):
     user_rating: float
 
 
-# ---------------- Load database ----------------
-books = pd.read_csv(r"BookRecomendationApp\books.csv")
+# ---------------- Connect to Mongo database ----------------
+MONGO_URI = "mongodb+srv://<db_username>:<db_password>@agilemtu.knrhvja.mongodb.net/?appName=AgileMTUers" # <--- Will need to update once the correct DB has been created
+db = client["UserDatabase"]             # <-- Will need to update once the correct DB has been created
+use_book_collection = db["book_ratings"]   
+
 
 # ---------------- Get User Book Ratings ----------------
 @app.get("/ratedBooks}", response_model = List[UserRatings])
@@ -21,12 +26,21 @@ def get_user_ratings(username_X: Optional[str] - Header(None)):
     if not username_X:
         raise  HTTPException(status_code=401, detail="Not authenticated")
     
-    user_book_ratings = books[
-        (books["username"].str.lower() == username_X.lower()) &
-        (books["user_rating"].between(1,5))
-    ]
+    user_book_ratings = list(use_book_collection.find(
+        {
+            "username": username_X.lower(),
+            "user_books" : {"$gte": 1, "$lte": 5}
+        }
+    ))
 
     if user_book_ratings.empty:
         raise  HTTPException(status_code=404, detail="You have no ratings yet!")
     
-    return user_book_ratings.to_dict(orient="records")
+    return [
+        {
+            "title": book["title"],
+            "author": book["author"],
+            "user_rating": book["user_rating"]
+        }
+        for book in user_book_ratings
+    ]
